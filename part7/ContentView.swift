83231c8bb8f87afcbfb981e5fd5dd428a50737af
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-09-16 14:33:17 +0900

    Initial Commit

---
+++ part7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  part7
+//
+//  Created by 伊藤浩之 on 2022/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ part7/part7App.swift
@@ -0,0 +1,17 @@
+//
+//  part7App.swift
+//  part7
+//
+//  Created by 伊藤浩之 on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct part7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
