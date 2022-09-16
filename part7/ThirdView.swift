7202dcf1343bd9d97e8a2e9eba95ddde61307453
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-09-16 14:55:42 +0900

    (Start)モーダル遷移

---
+++ part7/ThirdView.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdView.swift
+//  part7
+//
+//  Created by 伊藤浩之 on 2022/09/16.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
