e08b4f2b85aa7f042ff2b59b1ef3822ee4cf3ab5
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-09-16 14:34:42 +0900

    fast

---
+++ part7/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  part7
+//
+//  Created by 伊藤浩之 on 2022/09/16.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
